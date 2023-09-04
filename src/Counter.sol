ca011238c12592acb049324145c12f5b16184455
0xdagarn <sgnoo@naver.com>
2023-09-04 21:00:09 +0900

    inheritance/ constructor

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,10 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+import "forge-std/console.sol";
+
+contract A {
+    uint256 private _a;
+
+    constructor(uint256 a) {
+        console.log("A deployed");
+        _a = a;
+    }
+}
+
+contract B {
+    uint256 private _b;
+
+    constructor(uint256 b) {
+        console.log("B deployed");
+        _b = b;
+    }
+}
+
+contract Counter is A, B {
     uint256 public number;
 
+    constructor() A(1) B(2) {
+        console.log("Counter deployed");
+    }
+
     function setNumber(uint256 newNumber) public {
+        console.log("setNumber(%s)", newNumber);
         number = newNumber;
     }
 
